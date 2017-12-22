ee996ce4ff7efbfcff535eb0393ee48d8573eb5f
Wilfilio Alvarez <miswca@lawampm.com>
2017-12-22 14:48:59 -0500

    Add project files.

---
+++ FunFlacts/AllFlags.xaml.cs
@@ -0,0 +1,28 @@
+﻿using FlagData;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace FunFlacts
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class AllFlags : ContentPage
+    {
+        public AllFlags()
+        {
+            BindingContext = DependencyService.Get<FunFlactsViewModel>();
+            InitializeComponent();
+        }
+
+        async void OnItemTapped(object sender, ItemTappedEventArgs e)
+        {
+            DependencyService.Get<FunFlactsViewModel>().CurrentFlag = (Flag)e.Item;
+            await this.Navigation.PushAsync(new FlagDetailsPage());
+        }
+    }
+}
